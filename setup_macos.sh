48fada07b6f5e2be9e87886a5e6d323988d536ac
KOBAYASHI Daisuke <daisuke.koba@gmail.com>
2024-11-04 14:45:27 +0900

    setup: Add setup script for macOS.

---
+++ setup_macos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FILES=".gitconfig .inputrc .vim .vimrc .zprofile .zshrc .zshrc.d"
+
+for file in $FILES; do
+  if [ -e $HOME/$file ]; then
+    mv -v $HOME/$file $HOME/${file}.bak
+  fi
+  ln -sv $HOME/dotfiles/$file $HOME/$file
+done
